668710636346538ef341e736a1bdf3970ba9c164
Julia Henderson <yulia.p.henderson@gmail.com>
2024-04-03 09:43:02 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
         document.addEventListener("DOMContentLoaded", function () {
         const slider = document.querySelector('.slider');
         const slides = document.querySelectorAll('.slide');
@@ -24,43 +25,58 @@
     });
 
 
-    document.addEventListener('DOMContentLoaded', function() {
-        // Get reference to the scrollable container element
-        var container = document.querySelector('.container');
 
-        // Get reference to the button in the fixed header
-        var button = document.getElementById('scrollButton');
-         var mlogo = document.getElementById('m-logo');
-          var logo = document.getElementById('logo');
 
-        // Function to check if scrolled to the second page
-        function isScrolledToSecondPage() {
-            var secondPage = document.querySelector('.pages.page2');
-            if (secondPage) {
-                var containerHeight = container.clientHeight;
-                var scrollTop = container.scrollTop;
-                var secondPageOffsetTop = secondPage.offsetTop;
+document.addEventListener('DOMContentLoaded', function() {
+    // Get reference to the scrollable container element
+    var container = document.querySelector('.container');
 
-                return scrollTop >= secondPageOffsetTop - containerHeight / 2;
-            } else {
-                console.error('Second page element not found.');
-                return false;
-            }
+    // Get reference to the button in the fixed header
+    var header = document.getElementById('fixedHeader');
+    var logo = document.getElementById('mlogo');
+    var button = document.getElementById('scrollButton');
+
+
+    // Store the original logo source
+    var originalLogoSrc = logo.src;
+
+    // Function to check if scrolled to the second page
+    function isScrolledToSecondPage() {
+        var secondPage = document.querySelector('.pages.page2');
+        if (secondPage) {
+            var containerHeight = container.clientHeight;
+            var scrollTop = container.scrollTop;
+            var secondPageOffsetTop = secondPage.offsetTop;
+
+            return scrollTop >= secondPageOffsetTop - containerHeight / 4;
+        } else {
+            console.error('Second page element not found.');
+            return false;
         }
+    }
 
-        // Attach scroll event listener to the container
-        container.addEventListener('scroll', function() {
-            // Check if scrolled to the second page
-            if (isScrolledToSecondPage()) {
-                // Change button color when scrolled to the second page
-                button.style.color = '#ffffff';
-                button.style.backgroundColor = '#000000'; // Change to desired color
-            } else {
-                // Revert button color to default when not scrolled to the second page
-                button.style.color = '#000000';
-                button.style.backgroundColor = '#ffffff'; // Default button color
-            }
-        });
+    // Attach scroll event listener to the container
+    container.addEventListener('scroll', function() {
+        // Check if scrolled to the second page
+        if (isScrolledToSecondPage()) {
+            // Change button color when scrolled to the second page
+            header.style.backgroundColor = '#ffffff';
+            header.style.borderBottom = '1px solid #F4F4F4';
+            header.style.boxShadow = 'rgba(0, 0, 0, 0.1) 0px 2px 4px';
+            button.style.color = '#ffffff';
+            button.style.backgroundColor = '#000000'; // Change to desired color
+            logo.src = 'Images/logo_black.png'; // Change to the second logo image URL
+        } else {
+            // Revert button color to default when not scrolled to the second page
+            header.style.backgroundColor = 'transparent';
+            header.style.borderBottom = 'none';
+            header.style.boxShadow = 'none';
+            button.style.color = '#000000';
+            button.style.backgroundColor = '#ffffff'; // Default button color
+            logo.src = originalLogoSrc; // Revert to the original logo image
+        }
     });
+});
+
 
 
